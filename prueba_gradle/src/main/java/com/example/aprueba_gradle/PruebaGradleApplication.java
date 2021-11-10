9214508a670503fb04990c316f4c5393664916e0
Valeria Lavín <valeriavlgc@gmail.com>
2021-11-10 11:49:16 +0100

    Add files via upload
    Prueba Gradle

---
+++ prueba_gradle/src/main/java/com/example/aprueba_gradle/PruebaGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.aprueba_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGradleApplication.class, args);
+	}
+
+}
---
+++ prueba_gradle/src/test/java/com/example/prueba_gradle/PruebaGradleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.prueba_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class PruebaGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
